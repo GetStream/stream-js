545284b8fcded4732cd3d5d21b11895a84e2d2e5
Thierry Schellenbach <thierryschellenbach@gmail.com>
2014-06-23 10:12:03 +0200

    another attempt at sauce and travis

--- test/browser/runner.js
+++ test/browser/runner.js
@@ -1,21 +1,30 @@
 var MochaSauce = require("mocha-sauce");
+var connect = require('connect');
+var serveStatic = require('serve-static');
+var path = require('path');
+
+// setup a webserver to run the tests against
+var directory = path.normalize(path.join(__dirname, '../../'));
+console.log('starting webserver in dir ', directory);
+connect().use(serveStatic(directory)).listen(8080);
 
 // configure
 var sauce = new MochaSauce({
     name: "getstream", // your project name
     username: "tschellenbach", // Sauce username
     accessKey: "982137a0-d75d-4cd8-a6c3-1a497e97a277", // Sauce access key
-    host: "http://ondemand.sauce.com", // or http://ondemand.sauce.com if not using Sauce Connect
-    port: 80, // 80
+    host: "localhost", // or http://ondemand.sauce.com if not using Sauce Connect
+    port: 8080, // 80
     // the test url
-    url: "https://saucelabs.com/test_helpers/front_tests/index.html" // point to the site running your mocha tests
+    url: "http://localhost:8080/test/browser/sauce.html/" // point to the site running your mocha tests
 });
 
 
 // setup what browsers to test with
 sauce.browser({ browserName: "chrome", platform: "Windows 7" });
 sauce.browser({ browserName: "firefox", platform: "Windows XP" });
-
+sauce.browser({ browserName: "internet explorer", platform: "Windows 8.1", version: 11 });
+sauce.browser({ browserName: "internet explorer", platform: "Windows 8", version: 10 });
 
 sauce.on('init', function(browser) {
   console.log('  init : %s %s', browser.browserName, browser.platform);
