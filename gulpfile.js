9cfc6278972b907afb190e26e5be7a496769a7b0
Thierry Schellenbach <thierryschellenbach@gmail.com>
2014-06-23 11:10:53 +0200

    Release v0.1.9

--- gulpfile.js
+++ gulpfile.js
@@ -96,7 +96,7 @@ gulp.task('tag', function () {
 
   return gulp.src('./')
     .pipe(git.commit(message))
-    .on('error', gutil.log)
+    	.on('error', gutil.log)
     .pipe(git.tag(v, message))
     .pipe(git.push('origin', 'master', '--tags'))
     .pipe(gulp.dest('./'));
