bcb44fb165621aa40f499ffedcc6f64f0e1d8806
Tommaso Barbugli <tbarbugli@gmail.com>
2015-09-22 14:41:33 -0600

    make sure we transpile jsonwebtoken dep

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,7 @@ module.exports = {
     },
     module: {
       loaders: [
+        { test: /jsonwebtoken\/(.*)js/, loader: "babel-loader"},
         { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
       ]
     }
