fe7b340eb4bd7498e27a6f43a6d0ddde025d8723
Matthisk Heimensen <m@tthisk.nl>
2015-10-23 16:21:22 +0200

    Added jscs styleguide, fixed all styleguide issues

--- src/getstream.js
+++ src/getstream.js
@@ -8,32 +8,34 @@ var errors = require('./lib/errors');
 var request = require('request');
 
 function connect(apiKey, apiSecret, appId, options) {
-	/*
-	 * Usage
-	 * stream.connect(apiKey, apiSecret)
-	 * or if you want to be able to subscribe and listen
-	 * for changes
-	 * stream.connect(apiKey, apiSecret, appId)
-	 * or on heroku
-	 * stream.connect(streamURL)
-	 * where streamURL looks like this
-	 * https://thierry:pass@getstream.io/?app=1
-	 * 
-	 */
-	if (typeof(process) != "undefined" && process.env.STREAM_URL && !apiKey) {
-		var parts = /https\:\/\/(\w+)\:(\w+)\@([\w-]*).*\?app_id=(\d+)/.exec(process.env.STREAM_URL);
-		apiKey = parts[1];
-		apiSecret = parts[2];
-		var location = parts[3];
-		appId = parts[4];
-		if (options === undefined) {
-			options = {};
-		}
-		if (location != 'getstream') {
-			options.location = location;
-		}
-	}
-	return new StreamClient(apiKey, apiSecret, appId, options);
+  /*
+  	 * Usage
+  	 * stream.connect(apiKey, apiSecret)
+  	 * or if you want to be able to subscribe and listen
+  	 * for changes
+  	 * stream.connect(apiKey, apiSecret, appId)
+  	 * or on heroku
+  	 * stream.connect(streamURL)
+  	 * where streamURL looks like this
+  	 * https://thierry:pass@getstream.io/?app=1
+  	 *
+  	 */
+  if (typeof (process) !== 'undefined' && process.env.STREAM_URL && !apiKey) {
+    var parts = /https\:\/\/(\w+)\:(\w+)\@([\w-]*).*\?app_id=(\d+)/.exec(process.env.STREAM_URL);
+    apiKey = parts[1];
+    apiSecret = parts[2];
+    var location = parts[3];
+    appId = parts[4];
+    if (options === undefined) {
+      options = {};
+    }
+
+    if (location !== 'getstream') {
+      options.location = location;
+    }
+  }
+
+  return new StreamClient(apiKey, apiSecret, appId, options);
 }
 
 module.exports.connect = connect;
--- src/lib/parse_request.js
+++ src/lib/parse_request.js
@@ -1,26 +1,28 @@
 
 /*
- * Simple wrapper to make make parse httprequest look 
+ * Simple wrapper to make make parse httprequest look
  * somewhat like request library
  */
 
 function request(options, callback) {
-	// first difference with request, qs is called params
-	options.params = options.qs;
-	// next up we need to support json for complex body params
-	options.body = JSON.stringify(options.body);
-	// also the callback is somewhat different
-	function callbackWrapperSuccess(httpResponse) {
-		callback(httpResponse);
-	}
-	function callbackWrapperFailure(httpResponse) {
-		callback(httpResponse);
-	}
-	options.success = callbackWrapperSuccess;
-	options.error = callbackWrapperFailure;
-	// add the json header
-	options.headers['Content-Type'] = 'application/json;charset=utf-8';
-	Parse.Cloud.httpRequest(options);
+  // first difference with request, qs is called params
+  options.params = options.qs;
+  // next up we need to support json for complex body params
+  options.body = JSON.stringify(options.body);
+  // also the callback is somewhat different
+  function callbackWrapperSuccess(httpResponse) {
+    callback(httpResponse);
+  }
+
+  function callbackWrapperFailure(httpResponse) {
+    callback(httpResponse);
+  }
+
+  options.success = callbackWrapperSuccess;
+  options.error = callbackWrapperFailure;
+  // add the json header
+  options.headers['Content-Type'] = 'application/json;charset=utf-8';
+  Parse.Cloud.httpRequest(options);
 }
 
 module.exports = request;
