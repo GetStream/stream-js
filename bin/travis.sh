116ab9264ee7648ebbc4bf8bcb7b3e48b099243d
Matthisk Heimensen <m@tthisk.nl>
2016-10-10 16:30:55 +0200

    Run saucelabs on travis

---
+++ bin/travis.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+./node_modules/.bin/webpack
+npm test
+./bin/coveralls.sh
+if [ "$(node --version)" == "v6.7.0" ]; then 
+    ./node_modules/.bin/eslint src/**/*.js; 
+fi
+npm install -g bower
+bower install getstream
--- karma.config.js
+++ karma.config.js
@@ -31,15 +31,12 @@ module.exports = function(config) {
     };
 
     config.set({
-
         // base path that will be used to resolve all patterns (eg. files, exclude)
         basePath: '',
-
 
         // frameworks to use
         // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
         frameworks: ['mocha'],
-
 
         // list of files / patterns to load in the browser
         files: [
@@ -49,11 +46,9 @@ module.exports = function(config) {
           // 'test/integration/common/**/*_test.js',
         ],
 
-
         // list of files to exclude
         exclude: [
         ],
-
 
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
@@ -72,29 +67,25 @@ module.exports = function(config) {
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
         reporters: ['dots', 'saucelabs'],
 
-
         // web server port
         port: 9876,
 
-
         // enable / disable colors in the output (reporters and logs)
         colors: true,
-
 
         // level of logging
         // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
         logLevel: config.LOG_INFO,
 
-
         // enable / disable watching file and executing tests whenever any file changes
         autoWatch: true,
-
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
         // browsers: ['Chrome'],
         sauceLabs: {
             testName: 'Stream-JS Browser Tests',
+            public: "public",
         },
         customLaunchers: customLaunchers,
         browsers: Object.keys(customLaunchers),
