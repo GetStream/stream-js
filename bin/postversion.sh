a0038ea68688e4e7f2679269cfbd8821a6566bf7
Tommaso Barbugli <tbarbugli@gmail.com>
2017-10-19 14:24:01 +0200

    skip bower

--- bin/postversion.sh
+++ bin/postversion.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
-git add docs/getstream/
-git commit -m 'update docs'
-node ./bin/bower-up.js
-git add bower.json
-git commit -m 'publish new version on Bower' 
+#git add docs/getstream/
+#git commit -m 'update docs'
+#node ./bin/bower-up.js
+#git add bower.json
+#git commit -m 'publish new version on Bower' 
 git push origin master
 git push origin --tags
