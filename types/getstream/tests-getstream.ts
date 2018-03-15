1022a745f78f164f2954a5bbc1de2cce51f4b692
Dwight Gunning <dwight@dwightgunning.com>
2018-03-15 12:13:27 +0100

    Fixed missing TS type defintions for permutations of the overloaded unfollow function

--- types/getstream/tests-getstream.ts
+++ types/getstream/tests-getstream.ts
@@ -11,6 +11,11 @@ feed.follow('feedSlug', 'user', {activity_copy_limit: 10}); // $ExpectType Promi
 feed.follow('feedSlug', 'user', {activity_copy_limit: 10}, (err: object, httpResponse: object, body: object) => {}); // $ExpectType void
 feed.follow('feedSlug', 'user', (err: object, httpResponse: object, body: object) => {}); // $ExpectType void
 
+feed.unfollow('feedSlug', 'user'); // $ExpectType Promise<object>
+feed.unfollow('feedSlug', 'user', {keep_history: true}); // $ExpectType Promise<object>
+feed.unfollow('feedSlug', 'user', {keep_history: true}, (err: object, httpResponse: object, body: object) => {}); // $ExpectType void
+feed.unfollow('feedSlug', 'user', (err: object, httpResponse: object, body: object) => {}); // $ExpectType void
+
 new stream.errors.MissingSchemaError(); // $ExpectType MissingSchemaError
 new stream.errors.FeedError(); // $ExpectType FeedError
 new stream.errors.SiteError(); // $ExpectType SiteError
