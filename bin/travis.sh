693bb150f928cbbe4e896ec0028dd1919fa8c66c
Dwight Gunning <dwight@dwightgunning.com>
2018-05-09 16:27:31 +0200

    Update travis build config for Node 10

--- bin/travis.sh
+++ bin/travis.sh
@@ -3,8 +3,8 @@ set -e
 ./node_modules/.bin/webpack
 npm test
 ./bin/coveralls.sh
-if [ "$(node --version)" == "v6.7.0" ]; then 
-    ./node_modules/.bin/eslint src/**/*.js; 
+if [ "$(node --version)" == "v10.1.0" ]; then
+    ./node_modules/.bin/eslint src/**/*.js;
 fi
 npm install -g bower
 bower install getstream
