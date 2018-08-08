bc06be626a6d33b03396d5364cd6bca6924dfc60
Tommaso Barbugli <tbarbugli@gmail.com>
2018-08-08 15:51:15 +0200

    3.20.0-beta.5

--- dist/js/getstream.js
+++ dist/js/getstream.js
@@ -4366,7 +4366,9 @@ module.exports = StreamClient;
 "use strict";
 
 
-var _crossFetch = __webpack_require__(18);
+var _crossFetch = _interopRequireWildcard(__webpack_require__(18));
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
 
 function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
 
@@ -4400,7 +4402,7 @@ StreamImageStore.prototype = {
     }
 
     data.append('file', fileField);
-    return (0, _crossFetch.fetch)("".concat(this.client.enrichUrl('images/'), "?api_key=").concat(this.client.apiKey), {
+    return (0, _crossFetch.default)("".concat(this.client.enrichUrl('images/'), "?api_key=").concat(this.client.apiKey), {
       method: 'post',
       body: data,
       headers: new _crossFetch.Headers({
@@ -4454,7 +4456,9 @@ module.exports = StreamImageStore;
 "use strict";
 
 
-var _crossFetch = __webpack_require__(18);
+var _crossFetch = _interopRequireWildcard(__webpack_require__(18));
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
 
 var utils = __webpack_require__(10);
 
@@ -4486,7 +4490,7 @@ StreamFileStore.prototype = {
     }
 
     data.append('file', fileField);
-    return (0, _crossFetch.fetch)("".concat(this.client.enrichUrl('files/'), "?api_key=").concat(this.client.apiKey), {
+    return (0, _crossFetch.default)("".concat(this.client.enrichUrl('files/'), "?api_key=").concat(this.client.apiKey), {
       method: 'post',
       body: data,
       headers: new _crossFetch.Headers({
