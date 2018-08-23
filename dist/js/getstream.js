a2665cdb398fcbfc4aa0fa6960d7755b2bff167d
Jelte Fennema <github-tech@jeltef.nl>
2018-08-23 10:16:54 +0200

    3.21.0-beta.4

--- dist/js/getstream.js
+++ dist/js/getstream.js
@@ -4119,7 +4119,9 @@ StreamClient.prototype = {
      * @return {object} Faye client
      */
     if (this.fayeClient === null) {
-      this.fayeClient = new Faye.Client(this.fayeUrl);
+      this.fayeClient = new Faye.Client(this.fayeUrl, {
+        timeout: 10
+      });
       var authExtension = this.getFayeAuthorization();
       this.fayeClient.addExtension(authExtension);
     }
