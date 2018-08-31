591efa0f87b72c22499e0505ecb7ac4b9f55dafc
Jelte Fennema <github-tech@jeltef.nl>
2018-08-31 13:59:58 +0200

    Run cloud tests

--- bin/travis.sh
+++ bin/travis.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 set -e
 ./node_modules/.bin/webpack
-npm test
+if [ "${CLOUD_TESTS:='no'}" == "yes" ]; then
+    npm test-cloud
+else
+    npm test
+fi
 ./bin/coveralls.sh
-if [ ${RUN_LINTERS:="no"} == "yes" ]; then
+if [ "${RUN_LINTERS:='no'}" == "yes" ]; then
     ./node_modules/.bin/eslint src/**/*.js;
     # ./node_modules/.bin/dtslint types/getstream
 fi
