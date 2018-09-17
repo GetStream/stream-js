705a4beb2278df21be4fad070483e9ebf2181c2e
Jelte Fennema <github-tech@jeltef.nl>
2018-09-17 14:46:38 +0200

    Remove comment on no-useless-escape

--- src/getstream.js
+++ src/getstream.js
@@ -31,7 +31,7 @@ function connect(apiKey, apiSecret, appId, options) {
   if (typeof process !== 'undefined' && process.env.STREAM_URL && !apiKey) {
     var parts = /https:\/\/(\w+):(\w+)@([\w-]*).*\?app_id=(\d+)/.exec(
       process.env.STREAM_URL,
-    ); // eslint-disable-line no-useless-escape
+    );
     apiKey = parts[1];
     apiSecret = parts[2];
     var location = parts[3];
