0744ad0bcf08bb91b0ce075b8a917d8849259d97
Jelte Fennema <github-tech@jeltef.nl>
2018-10-12 17:05:47 +0200

    Fix personalization tests

--- test/integration/cloud/personalized_feed.js
+++ test/integration/cloud/personalized_feed.js
@@ -4,13 +4,24 @@ describe('Personalized enrichment story', () => {
   let ctx = new CloudContext();
 
   describe('When alice reads her personalization feed', () => {
-    ctx.requestShouldError(404, async () => {
-      ctx.response = await ctx.alice.personalizedFeed({ feed_slug: 'abc' });
+    ctx.requestShouldError(400, async () => {
+      ctx.response = await ctx.alice.personalizedFeed({
+        feed_slug: 'timeline',
+      });
     });
   });
   describe('When alice reads her personalization feed without args', () => {
-    ctx.requestShouldError(404, async () => {
+    ctx.requestShouldError(400, async () => {
       ctx.response = await ctx.alice.personalizedFeed();
+    });
+  });
+  describe('When alice reads her personalization feed', () => {
+    ctx.requestShouldError(404, async () => {
+      ctx.response = await ctx.alice.personalizedFeed({
+        feed_slug: 'timeline',
+        userId: 'hello',
+        endpoint: 'not_existing',
+      });
     });
   });
 });
