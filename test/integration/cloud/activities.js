c69c7b64224aee00053bdc2ef45a1e01a8910f17
Jelte Fennema <github-tech@jeltef.nl>
2018-12-24 14:07:08 +0100

    Support enriched getActivities

--- test/integration/cloud/activities.js
+++ test/integration/cloud/activities.js
@@ -4,6 +4,8 @@ describe('Get activities', () => {
   let ctx = new CloudContext();
   ctx.createUsers();
   ctx.aliceAddsCheeseBurger();
+  let originalActivity;
+  let enrichedActivity;
 
   describe('When alice eats the cheese burger', () => {
     let at = new Date().toISOString();
@@ -15,19 +17,124 @@ describe('Get activities', () => {
         foreign_id: 'fid:123',
         time: at,
       });
+      originalActivity = ctx.response;
     });
+  });
 
-    it('Should be possible to read the same activity by ID', async () => {
-      let response = await ctx.alice
+  describe('When getting the activity from the feed with getActivityDetail', async () => {
+    ctx.requestShouldNotError(async () => {
+      ctx.response = await ctx.alice
         .feed('user')
-        .getActivityDetail([ctx.response.id]);
-      let activity = response.results[0];
-      activity.should.have.property('foreign_id');
-      ctx.shouldEqualBesideDuration(activity.actor, ctx.alice.currentUser.full);
-      activity.verb.should.equal('eat');
-      ctx.shouldEqualBesideDuration(activity.object, ctx.cheeseBurger.full);
-      activity.object.collection.should.equal('food');
-      // activity.object.data.name.should.equal('cheese burger');
+        .getActivityDetail(originalActivity.id);
+    });
+    ctx.responseShouldHaveActivityWithFields();
+    ctx.activityShould('have enriched data', () => {
+      ctx.activity.id.should.equal(originalActivity.id);
+      ctx.activity.foreign_id.should.equal('fid:123');
+      ctx.shouldEqualBesideDuration(
+        ctx.activity.actor,
+        ctx.alice.currentUser.full,
+      );
+      ctx.activity.verb.should.equal('eat');
+      ctx.shouldEqualBesideDuration(ctx.activity.object, ctx.cheeseBurger.full);
+      enrichedActivity = ctx.activity;
+    });
+  });
+
+  describe('When getting the activity without feed by ID', async () => {
+    ctx.requestShouldNotError(async () => {
+      ctx.response = await ctx.alice.getActivities({
+        ids: [originalActivity.id],
+        enrich: true,
+      });
+    });
+    ctx.responseShould('have a single activity', () => {
+      ctx.response.should.have.all.keys('results', 'duration');
+      ctx.response.results.should.be.lengthOf(1);
+      ctx.activity = ctx.response.results[0];
+    });
+    ctx.activityShould('have enriched data', () => {
+      ctx.activity.should.eql(enrichedActivity);
+    });
+  });
+
+  describe('When getting the activity without feed by ID', async () => {
+    ctx.requestShouldNotError(async () => {
+      ctx.response = await ctx.alice.getActivities({
+        ids: [originalActivity.id],
+      });
+    });
+    ctx.responseShould('have a single activity', () => {
+      ctx.response.should.have.all.keys('results', 'duration');
+      ctx.response.results.should.be.lengthOf(1);
+      ctx.activity = ctx.response.results[0];
+    });
+    ctx.activityShould('have enriched data', () => {
+      ctx.activity.should.eql(enrichedActivity);
+    });
+  });
+
+  describe('When getting the activity without feed by ID', async () => {
+    ctx.requestShouldNotError(async () => {
+      ctx.response = await ctx.alice.getActivities({
+        ids: [originalActivity.id],
+      });
+    });
+    ctx.responseShould('have a single activity', () => {
+      ctx.response.should.have.all.keys('results', 'duration');
+      ctx.response.results.should.be.lengthOf(1);
+      ctx.activity = ctx.response.results[0];
+    });
+    ctx.activityShould('have enriched data', () => {
+      ctx.activity.should.eql(enrichedActivity);
+    });
+  });
+
+  describe('When getting the activity without feed by ID', async () => {
+    ctx.requestShouldNotError(async () => {
+      ctx.response = await ctx.alice.getActivities({
+        ids: [originalActivity.id],
+      });
+    });
+    ctx.responseShould('have a single activity', () => {
+      ctx.response.should.have.all.keys('results', 'duration');
+      ctx.response.results.should.be.lengthOf(1);
+      ctx.activity = ctx.response.results[0];
+    });
+    ctx.activityShouldHaveFields();
+    ctx.activityShould('have enriched data', () => {
+      ctx.activity.should.eql(enrichedActivity);
+    });
+  });
+
+  describe('When getting the activity without feed by ID and with full enrichment', async () => {
+    ctx.requestShouldNotError(async () => {
+      ctx.response = await ctx.alice.getActivities({
+        ids: [originalActivity.id],
+        reactions: {
+          own: true,
+          recent: true,
+          counts: true,
+        },
+      });
+    });
+    ctx.responseShould('have a single activity', () => {
+      ctx.response.should.have.all.keys('results', 'duration');
+      ctx.response.results.should.be.lengthOf(1);
+      ctx.activity = ctx.response.results[0];
+    });
+    ctx.activityShouldHaveFields(
+      'latest_reactions',
+      'latest_reactions_extra',
+      'own_reactions',
+      'reaction_counts',
+    );
+    ctx.activityShould('have enriched data', () => {
+      ctx.activity.id.should.equal(originalActivity.id);
+      ctx.activity.latest_reactions.should.eql({});
+      ctx.activity.latest_reactions_extra.should.eql({});
+      ctx.activity.own_reactions.should.eql({});
+      ctx.activity.reaction_counts.should.eql({});
     });
   });
 });
