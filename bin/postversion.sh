0bbf4f85d98ba262912f7e1930d009cb04738385
ferhat elmas <elmas.ferhat@gmail.com>
2020-03-20 12:12:20 +0100

    Drop bower already disabled (#284)

--- bin/postversion.sh
+++ bin/postversion.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 set -e
-#git add docs/getstream/
-#git commit -m 'update docs'
-#node ./bin/bower-up.js
-#git add bower.json
-#git commit -m 'publish new version on Bower' 
 git push
 git push --tags
