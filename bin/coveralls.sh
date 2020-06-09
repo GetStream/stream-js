c8787c33591eed40c0354061cf11f311d26e70f2
ferhat elmas <elmas.ferhat@gmail.com>
2020-06-09 11:59:57 +0200

    Correct coverage from package.json

--- bin/coveralls.sh
+++ bin/coveralls.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-nyc  --reporter=lcov mocha --require @babel/register bin/run-node-unit-tests.js
+npm run coverage
 cat ./coverage/lcov.info | ./node_modules/.bin/coveralls
 rm -rf ./coverage
