581e3911f24f150d05f080bea35da64c453e37f0
Amin Mahboubi <aminmahboobi@gmail.com>
2020-10-26 15:36:43 +0100

    test: unit test image store

---
+++ test/unit/common/images_test.js
@@ -0,0 +1,83 @@
+import expect from 'expect.js';
+import td from 'testdouble';
+
+import { StreamImageStore } from '../../../src';
+import { init, beforeEachFn } from '../utils/hooks';
+
+describe('[UNIT] Images (Common)', function () {
+  const uri = 'file_url';
+  let get;
+  let post;
+  let del;
+  let store;
+
+  init.call(this);
+  beforeEach(beforeEachFn);
+  beforeEach(function () {
+    store = new StreamImageStore(this.client, 'token');
+    post = td.function();
+    del = td.function();
+    get = td.function();
+
+    td.replace(this.client, 'get', get);
+    td.replace(this.client, 'post', post);
+    td.replace(this.client, 'delete', del);
+  });
+
+  afterEach(function () {
+    td.reset();
+  });
+
+  it('#initialize', function () {
+    expect(store.client).to.be(this.client);
+    expect(store.token).to.be('token');
+  });
+
+  it('#refreshUrl', function () {
+    store.refreshUrl(uri);
+    td.verify(
+      post({
+        url: 'images/refresh/',
+        body: { url: uri },
+        signature: 'token',
+      }),
+    );
+  });
+
+  it('#process', function () {
+    const options = { crop: 'bottom', resize: 'scale', h: 100, w: 100 };
+    store.process(uri, options);
+
+    td.verify(
+      get({
+        url: 'images/',
+        qs: { url: uri, ...options },
+        signature: 'token',
+      }),
+    );
+  });
+
+  it('#thumbnail', function () {
+    store.thumbnail(uri, '200', '200');
+
+    td.verify(
+      get({
+        url: 'images/',
+        qs: { url: uri, crop: 'center', resize: 'clip', h: '200', w: '200' },
+        signature: 'token',
+      }),
+    );
+  });
+
+  it('#delete', function () {
+    store.delete(uri);
+
+    td.verify(
+      del({
+        url: 'images/',
+        qs: { url: uri },
+        signature: 'token',
+      }),
+    );
+  });
+});
