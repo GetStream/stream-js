57f23b67d5dd7a3c33f42c5edf4f8b447ddad9d1
Amin Mahboubi <amin@getstream.io>
2021-04-22 15:51:25 +0200

    Revert "Fix: follow stats defaults to feed token" (#446)
    This reverts commit 963bc23c8f151437aae155e20c0b848108846e00.

--- src/feed.ts
+++ src/feed.ts
@@ -467,7 +467,7 @@ export class StreamFeed<
     return this.client.get<FollowStatsAPIResponse>({
       url: 'stats/follow/',
       qs,
-      token: this.token,
+      token: this.client.getOrCreateToken() || this.token,
     });
   }
 
