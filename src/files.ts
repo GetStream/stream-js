e79036bf69c926b603ead0e59cfd2e048b791a55
Tiago <1585571+xernobyl@users.noreply.github.com>
2023-11-21 10:24:54 -0800

    [PBE-000] downgrade axios version (react-native issue) (#584)
    * chore: downgrade axios version
    
    * chore: fix missing import

--- src/files.ts
+++ src/files.ts
@@ -1,4 +1,4 @@
-import { AxiosProgressEvent } from 'axios';
+import AxiosProgressEvent from 'axios';
 import { StreamClient } from './client';
 
 export class StreamFileStore {
@@ -25,7 +25,7 @@ export class StreamFileStore {
     uri: string | File | Buffer | NodeJS.ReadStream,
     name?: string,
     contentType?: string,
-    onUploadProgress?: (progressEvent: AxiosProgressEvent) => void,
+    onUploadProgress?: (progressEvent: typeof AxiosProgressEvent) => void,
   ) {
     return this.client.upload('files/', uri, name, contentType, onUploadProgress);
   }
