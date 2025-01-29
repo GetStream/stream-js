38ace4bc34c084eb151b57e7e57f36870a50158f
tatsuyayamauchi <tatsuya.yamauchi.us@gmail.com>
2025-01-29 09:55:17 +0900

    replace: jsonwebtoken to tsndr/cloudflare-worker-jwt

--- src/signing.ts
+++ src/signing.ts
@@ -1,4 +1,4 @@
-import jwt, { SignOptions } from 'jsonwebtoken';
+import jwt from '@tsndr/cloudflare-worker-jwt';
 
 // for a claim in jwt
 function joinClaimValue(items: string | string[]): string {
@@ -33,14 +33,15 @@ export function JWTScopeToken(
   options: { expireTokens?: boolean; feedId?: string | string[]; userId?: string } = {},
 ) {
   const noTimestamp = options.expireTokens ? !options.expireTokens : true;
-  const payload: { action: string; resource: string; feed_id?: string; user_id?: string } = {
+  const payload: { action: string; resource: string; exp?: number; feed_id?: string; user_id?: string } = {
     resource: joinClaimValue(resource),
     action: joinClaimValue(action),
   };
   if (options.feedId) payload.feed_id = joinClaimValue(options.feedId);
   if (options.userId) payload.user_id = options.userId;
+  if (!noTimestamp) payload.exp = Math.floor(Date.now() / 1000) + 24 * (60 * 60); // Expires: Now + 24h;
 
-  return jwt.sign(payload, apiSecret, { algorithm: 'HS256', noTimestamp });
+  return jwt.sign(payload, apiSecret, { algorithm: 'HS256' });
 }
 
 /**
@@ -51,21 +52,13 @@ export function JWTScopeToken(
  * @param {string} apiSecret - API Secret key
  * @param {string} userId - The user_id key in the JWT payload
  * @param {object} [extraData] - Extra that should be part of the JWT token
- * @param {object} [jwtOptions] - Options that can be past to jwt.sign
  * @return {string} JWT Token
  */
-export function JWTUserSessionToken(
-  apiSecret: string,
-  userId: string,
-  extraData: Record<string, unknown> = {},
-  jwtOptions: SignOptions = {},
-) {
+export function JWTUserSessionToken(apiSecret: string, userId: string, extraData: Record<string, unknown> = {}) {
   if (typeof userId !== 'string') {
     throw new TypeError('userId should be a string');
   }
-
   const payload = { user_id: userId, ...extraData };
 
-  const opts: SignOptions = { algorithm: 'HS256', noTimestamp: true, ...jwtOptions };
-  return jwt.sign(payload, apiSecret, opts);
+  return jwt.sign(payload, apiSecret, { algorithm: 'HS256' });
 }
